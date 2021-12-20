bb2888538dc784d257f349519455f072adb02724
taekyeongwon <tkw3351@gmail.com>
2021-12-20 15:24:59 +0900

    언어 설정 추가

---
+++ MyTestApp/app/src/main/java/com/tkw/kr/myapplication/MainApplication.kt
@@ -0,0 +1,57 @@
+package com.tkw.kr.myapplication
+
+import android.content.Context
+import android.os.Process
+import androidx.multidex.MultiDexApplication
+import com.tkw.kr.myapplication.core.helper.LocaleHelper
+import com.tkw.kr.myapplication.util.Logger
+import java.io.PrintWriter
+import java.io.StringWriter
+
+class MainApplication: MultiDexApplication() {
+    companion object {
+        lateinit var application: MainApplication
+
+        fun getString(stringResId: Int): String {
+            return application.getString(stringResId) ?: ""
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        application = this
+        setUncaughtExceptionHandler()
+    }
+
+    private fun setUncaughtExceptionHandler() {
+        val defaultHandler: Thread.UncaughtExceptionHandler? = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            Logger.e("${thread.name} + : + ${getStackTrace(throwable)}")
+
+            defaultHandler?.uncaughtException(thread, throwable)
+            Process.killProcess(Process.myPid())
+            System.exit(0)
+        }
+    }
+
+    private fun getStackTrace(e: Throwable): String {
+        val result = StringWriter()
+        val printWriter = PrintWriter(result)
+
+        var th: Throwable? = e
+        while(th != null) {
+            th.printStackTrace(printWriter)
+            th = th.cause
+        }
+
+        val stackTraceAsString = result.toString()
+        printWriter.close()
+
+        return stackTraceAsString
+    }
+
+    override fun attachBaseContext(base: Context) {
+        val lang = LocaleHelper.getSelectedLanguage(base, "")
+        super.attachBaseContext(LocaleHelper.getUpdateContext(base, lang))
+    }
+}
---
+++ MyTestApp/app/src/main/java/com/tkw/kr/myapplication/core/constant/C.kt
@@ -0,0 +1,8 @@
+package com.tkw.kr.myapplication.core.constant
+
+object C {
+    object Preference {
+        const val SELECTED_LANGUAGE = "Locale.Helper.Selected.Language"
+        const val FIRST_LANGUAGE_SELECTED = "FIRST_LANGUAGE_SELECTED"
+    }
+}
