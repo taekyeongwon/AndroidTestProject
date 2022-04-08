d36099ea0781116f103cbb0780a7d17e4f039a57
taekyeongwon <tkw3351@gmail.com>
2022-04-08 15:57:30 +0900

    카카오, 네이버 viewModel 초기화 누락되어 추가

--- MyTestApp/app/src/main/java/com/tkw/kr/myapplication/component/map/KakaoMapActivity.kt
+++ MyTestApp/app/src/main/java/com/tkw/kr/myapplication/component/map/KakaoMapActivity.kt
@@ -1,8 +1,10 @@
 package com.tkw.kr.myapplication.component.map
 
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.tkw.kr.myapplication.R
 import com.tkw.kr.myapplication.base.BaseView
+import com.tkw.kr.myapplication.core.factory.MyProviderFactory
 import kotlinx.android.synthetic.main.activity_kakaomap.*
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
@@ -13,6 +15,7 @@ class KakaoMapActivity: BaseView<MapViewModel>() {
     override lateinit var viewModel: MapViewModel
 
     override fun initView() {
+        viewModel = ViewModelProvider(this, MyProviderFactory()).get(MapViewModel::class.java)
         val mapView = MapView(this)
         val mapViewContainer = map_view_kakao as ViewGroup
         mapViewContainer.addView(mapView)
--- MyTestApp/app/src/main/java/com/tkw/kr/myapplication/component/map/NaverMapActivity.kt
+++ MyTestApp/app/src/main/java/com/tkw/kr/myapplication/component/map/NaverMapActivity.kt
@@ -1,5 +1,6 @@
 package com.tkw.kr.myapplication.component.map
 
+import androidx.lifecycle.ViewModelProvider
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.MapFragment
@@ -7,6 +8,7 @@ import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.tkw.kr.myapplication.R
 import com.tkw.kr.myapplication.base.BaseView
+import com.tkw.kr.myapplication.core.factory.MyProviderFactory
 
 class NaverMapActivity: BaseView<MapViewModel>(), OnMapReadyCallback {
     override val layoutResourceId: Int
@@ -14,6 +16,7 @@ class NaverMapActivity: BaseView<MapViewModel>(), OnMapReadyCallback {
     override lateinit var viewModel: MapViewModel
 
     override fun initView() {
+        viewModel = ViewModelProvider(this, MyProviderFactory()).get(MapViewModel::class.java)
         val fm = supportFragmentManager
         val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
             ?: MapFragment.newInstance().also {
